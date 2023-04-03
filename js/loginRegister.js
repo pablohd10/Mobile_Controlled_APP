b1c4e7c6b07d3b4fb0744681615aba579b1a89c1
100451225 <100451225@alumnos.uc3m.es>
2023-04-03 12:21:59 +0200

    openPaginaPrincipal()

--- js/loginRegister.js
+++ js/loginRegister.js
@@ -4,3 +4,8 @@ function openRegister(){
 function openLogin(){
     window.open("iniciarSesion.html", "_self")
 }
+function openPaginaPrincipal(){
+    window.open("paginaPrincipal.html", "_self")
+}
+
+
